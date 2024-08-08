0fe75720c3498386ff1e90fe7d577df1cd45698d
promptaction8 <jinwoo30754@naver.com>
2024-08-08 18:56:17 +0900

    -ing

--- src/pages/components/dashboardcomponents.tsx
+++ src/pages/components/dashboardcomponents.tsx
@@ -13,25 +13,25 @@ function DashboardComponents() {
             <div className="flex flex-grow p-4 ">
                 <div className="flex flex-col w-1/5 pr-8  ml-50">
                     {/* 왼쪽 상단 작은 박스 */}
-                    <div className="bg-white shadow-lg p-3 mb-20 h-80">
+                    <div className="bg-white shadow-lg p-10 mb-20 h-80 rounded-xl ">
                         작은 박스
-                    </div>{' '}
+                    </div>
                     {/* 긴 박스 */}
-                    <div className="bg-white shadow-lg p-3 flex-grow mb-8">
+                    <div className="bg-white shadow-lg p-10 flex-grow mb-8 rounded-xl">
                         긴 박스
-                    </div>{' '}
+                    </div>
                 </div>
                 <div className="flex flex-col w-3/4  px-40">
                     {/* 오른쪽 박스 3개 */}
-                    <div className="bg-white shadow-lg p-3 mb-8 flex-1">
+                    <div className="bg-white shadow-lg p-10 mb-8 flex-1 rounded-xl">
                         박스 1
-                    </div>{' '}
-                    <div className="bg-white shadow-lg p-3 mb-8 flex-1">
+                    </div>
+                    <div className="bg-white shadow-lg p-10 mb-8 flex-1 rounded-xl">
                         박스 2
-                    </div>{' '}
-                    <div className="bg-white shadow-lg p-3 mb-8 flex-1">
+                    </div>
+                    <div className="bg-white shadow-lg p-10 mb-8 flex-1 rounded-xl">
                         박스 3
-                    </div>{' '}
+                    </div>
                 </div>
             </div>
         </div>

c985c6354a787106d9f4ae21fb4b462efea06cf5
promptaction8 <jinwoo30754@naver.com>
2024-09-05 23:43:49 +0900

    dao 에러

--- src/dao/patientRegister.ts
+++ src/dao/patientRegister.ts
@@ -112,6 +112,13 @@ export const patientRegister = async (data: IPatientRegister) => {
             patientIdx: patient.idx,
         },
     })
-    // 두번 넣는거 안됨?
-    return { patient, insurance, medicalHistory }
+    // 두번 넣는거 안됨? 됨?
+    const nursingSurvey = await prisma.nursingSurvey.create({
+        data: {
+            name,
+            gender,
+            patientIdx: patient.idx,
+        },
+    })
+    return { patient, insurance, medicalHistory, nursingSurvey }
 }

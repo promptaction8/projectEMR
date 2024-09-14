5d7f983302042cd54691f26fabe4160239ceba10
promptaction8 <126854470+promptaction8@users.noreply.github.com>
2024-09-14 16:28:25 +0000

    css

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -9,31 +9,35 @@ const LoginF: React.FC = () => {
     };
 
     return (
-        <div className='w-full h-full flex flex-col justify-center items-center dark:text-white'>
-            <div className='flex flex-col justify-center items-center w-120 '>
-                <span className='mb-20 text-7xl font-bold text-[#0EA5E9]'>Login</span>
+        <div className='w-full h-full flex flex-col justify-center items-center dark:text-white '>
+<div className='border-2 border-[#0ea7e9] border-solid p-40 rounded-lg'>
+<div className='w-120 flex flex-col justify-center items-center '>
+                <span className='text-5xl font-bold text-[#0EA5E9] mb-20'>Login</span>
                 {/* 그라데이션 밑줄 추가 */}
-                <div className='left-0 right-0 bottom-0 mb-60'>
+                <div className='left-0 right-0 bottom-0 mb-20'>
                     <div className='h-1 w-120  bg-gradient-to-r from-transparent via-blue-600 to-transparent'></div>
                 </div>
-                <div className='flex flex-row gap-36'>
 
-                <button
-                className='text-2xl font-bold text-[#0EA5E9] relative mb-4 mt-10 border-4 border-double border-[#0EA5E9] p-5 rounded-md'
+                <div className='flex flex-row justify-between w-full'>
+                    <button
+                className=' text-2xl font-bold text-[#0EA5E9] relative  mt-10 border-t-2 border-l-2 border-r-2 border-[#0EA5E9] p-8 rounded-t-md'
                 onClick={() => handleTabClick(true)} style={{ fontWeight: isEmployeeLogin ? 'bold'  : 'normal' }}>
                     직원 로그인
-                </button>
-                <button
-                className='mt-10 text-2xl font-bold text-[#0EA5E9] relative mb-4 border-4 border-double border-[#0EA5E9] p-5 rounded-md'
+                    </button>
+                    <button
+                className='mt-10 text-2xl font-bold text-[#0EA5E9] relative  border-t-2 border-l-2 border-r-2 border-[#0EA5E9] p-8 rounded-t-md'
                 onClick={() => handleTabClick(false)} style={{ fontWeight: !isEmployeeLogin ? 'bold' : 'normal' }}>
                     환자 로그인
-                </button>
+                    </button>
                 </div>
 
             </div>
             <LoginState isEmployeeLogin={isEmployeeLogin}/>
         </div>
+</div>
+
     );
-};
+}
+
 
 export default LoginF;
--- src/components/login/loginState.tsx
+++ src/components/login/loginState.tsx
@@ -1,4 +1,4 @@
-import React from 'react'  
+import React from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form'
 import { useMutation } from '@tanstack/react-query'
 import { useRouter } from 'next/router'
@@ -12,6 +12,9 @@ import 'react-responsive-modal/styles.css'
 import EmployeeAccount from '../loginpagecomponents/modal/employeeAccount'
 import { useQuery } from '@tanstack/react-query'
 import FindEmployeePassword from '../loginpagecomponents/modal/employeePassword';
+import { FiLogIn } from 'react-icons/fi'
+
+
 
 interface IEmployeeLoginData {
     id: string
@@ -75,18 +78,19 @@ export function LoginState({isEmployeeLogin}: {isEmployeeLogin: boolean}) {
         await employeeLogin.mutate(data)
     }
     return(
-        <div className='w-120 p-20'>
+        <div className='w-120 p-20 border-2 border-[#0ea7e9] border-solid'>
                     <h2 className='font-bold text-xl text-[#0ea5e9]'>{isEmployeeLogin === true ? "직원": "환자"} 로그인</h2>
                     <form onSubmit={handleSubmit(login)}>
-                        <div className='my-10'>
+                        <div className='my-10 relative w-full'>
                             <label>
                             <input
                             {...register('id', { required: true })}
                             maxLength={20}
                             placeholder='아이디'
-                            className='border-2 border-solid border-[#0EA5E9] rounded-t-lg p-10 w-full'
+                            className='border-2 border-solid border-[#0EA5E9] rounded-t-lg p-10 w-full text-black font-mono'
                             type="text" name="patientId" />
                             </label>
+                            <FiLogIn className='absolute right-0 top-0 mt-10 mr-10 text-[#0EA5E9]'/>
                             {errors.id && <span>아이디를 입력해주세요</span>}
                         </div>
                         <div className='my-10'>
@@ -94,24 +98,13 @@ export function LoginState({isEmployeeLogin}: {isEmployeeLogin: boolean}) {
                             <input
                             {...register('password', { required: true })}
                             placeholder='비밀번호'
-                            className='border-2 border-solid border-[#0EA5E9] rounded-b-lg p-10 w-full'
+                            className='border-2 border-solid border-[#0EA5E9] rounded-b-lg p-10 w-full text-black font-mono'
                             type="password" name="patientPassword" />
                             </label>
                             {errors.password && <span>비밀번호를 입력해주세요</span>}
                         </div>
-                        <button
-                                    disabled={employeeLogin.isPending}
-                                    className="bg-white text-[#0ea7e9] border-[#0ea7e9] border-2 border-solid rounded-md h-12 p-3 mt-20 hover:bg-[#0A74B9] hover:text-white transition duration-300 w-full"
-                                >
-                                    {employeeLogin.isPending ? (
-                                        <div className="flex items-center justify-center">
-                                            <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-white dark:animate-spin dark:border-b-2 dark:border-white"></div>
-                                        </div>
-                                    ) : (
-                                        '로그인'
-                                    )}
-                                </button>
-                                <div className='flex flex-row justify-center items-center gap-16 mt-8'>
+
+                        <div className='flex flex-row justify-center items-center gap-16 mt-8'>
                                     <p className="text-sm mt-10 cursor-pointer" onClick={handleOpenModal3}>비밀번호 찾기</p>
                                     <Modal open={isModalOpen3} onClose={handleCloseModal3} center>
                                         <FindEmployeePassword />
@@ -126,6 +119,20 @@ export function LoginState({isEmployeeLogin}: {isEmployeeLogin: boolean}) {
 
                                     </Modal>
                                 </div>
+
+                        <button
+                                    disabled={employeeLogin.isPending}
+                                    className="bg-white text-[#0ea7e9] border-[#0ea7e9] border-2 border-solid rounded-md h-12 p-3 mt-20 hover:bg-[#0A74B9] hover:text-white transition duration-300 w-full"
+                                >
+                                    {employeeLogin.isPending ? (
+                                        <div className="flex items-center justify-center">
+                                            <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-white dark:animate-spin dark:border-b-2 dark:border-white"></div>
+                                        </div>
+                                    ) : (
+                                        '로그인'
+                                    )}
+                                </button>
+
 
 
                     </form>
